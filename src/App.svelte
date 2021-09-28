cda5d46c7ac7734a516203c720bcb91a8ba0cf9c
lume100 <lukas.meihsner@gmail.com>
2021-09-28 19:29:20 +0100

    test 2

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello Mr Sofort {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps. Some more stuff</p>
 </main>
 
 <style>
